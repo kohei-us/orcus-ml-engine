3f7c82f4ff40f5979066322a3b192766b2ea4665
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-07-02 13:43:13 -0400

    Move the formula xml processor code into its own files.

---
+++ formula-correction/src/formula_xml_processor.hpp
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "trie_builder.hpp"
+
+#include <boost/filesystem.hpp>
+#include <string>
+#include <vector>
+
+class formula_xml_processor
+{
+    trie_builder m_trie;
+    boost::filesystem::path m_outdir;
+    const bool m_verbose;
+
+    void parse_file(const std::string& filepath);
+
+public:
+    formula_xml_processor(const boost::filesystem::path& outdir, bool verbose);
+
+    formula_xml_processor(const formula_xml_processor&) = delete;
+
+    void parse_files(const std::vector<std::string>& filepaths);
+
+    void write_files();
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
