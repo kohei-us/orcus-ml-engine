40b5d8e7160d57df26e357202dfc57e6427bb581
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-10-06 17:05:27 -0400

    Let's remove the version that uses xml.etree.
    The memory footprint is too high for it to be usable.

--- formula-correction/misc/extract-formulas.py
+++ formula-correction/misc/extract-formulas.py
@@ -47,10 +47,6 @@ def escape_str(s):
 
 
 def process_document(filepath, doc):
-    return process_document_direct_xml(filepath, doc)
-
-
-def process_document_direct_xml(filepath, doc):
 
     def write_tokens(iter, f):
         for token in iter:
@@ -103,99 +99,6 @@ def process_document_direct_xml(filepath, doc):
         f.write("</doc>")
 
         return output_buffer
-
-
-def process_document_xml_etree(filepath, doc):
-    import xml.etree.ElementTree as ET
-
-    def process_formula_tokens(obj):
-        formula_tokens = list()
-        for token in obj.get_formula_tokens():
-            formula_tokens.append([str(token), to_string(token.type)])
-        return formula_tokens
-
-    def add_named_expression(parent, name, exp, add_tokens, scope):
-        elem = ET.SubElement(parent, "named-expression")
-        elem.attrib["name"] = name
-        elem.attrib["formula"] = exp.formula
-        elem.attrib["scope"] = scope
-        elem.attrib["origin"] = exp.origin
-
-        if add_tokens:
-            tokens = process_formula_tokens(exp)
-            for token in tokens:
-                elem_token = ET.SubElement(elem, "token")
-                elem_token.attrib["s"] = token[0]
-                elem_token.attrib["type"] = token[1]
-
-    add_tokens = False
-    output_buffer = list()
-
-    elem_doc = ET.Element("doc")
-    elem_doc.attrib["filepath"] = filepath
-    elem_sheets = ET.SubElement(elem_doc, "sheets")
-    elem_sheets.attrib["count"] = str(len(doc.sheets))
-
-    elem_named_exps = ET.SubElement(elem_doc, "named-expressions")
-
-    for name, exp in doc.get_named_expressions():
-        add_named_expression(elem_named_exps, name, exp, add_tokens, "global")
-
-    elem_formulas = ET.SubElement(elem_doc, "formulas")
-
-    for sheet in doc.sheets:
-        for name, exp in sheet.get_named_expressions():
-            add_named_expression(elem_named_exps, name, exp, add_tokens, "sheet")
-
-        elem = ET.SubElement(elem_sheets, "sheet")
-        elem.attrib["name"] = sheet.name
-        output_buffer.append(f"* sheet: {sheet.name}")
-
-        for row_pos, row in enumerate(sheet.get_rows()):
-            for col_pos, cell in enumerate(row):
-                data = dict(sheet=sheet.name, row=row_pos, column=col_pos)
-                if cell.type == orcus.CellType.FORMULA:
-                    data.update({
-                        "valid": True,
-                        "formula": cell.formula,
-                    })
-
-                    if add_tokens:
-                        data["formula_tokens"] = process_formula_tokens(cell)
-
-                elif cell.type == orcus.CellType.FORMULA_WITH_ERROR:
-                    tokens = [str(x) for x in cell.get_formula_tokens()]
-                    data.update({
-                        "valid": False,
-                        "formula": tokens[1],  # original formula string
-                        "error": tokens[2],
-                    })
-                else:
-                    continue
-
-                elem = ET.SubElement(elem_formulas, "formula")
-                elem.attrib["sheet"] = data["sheet"]
-                elem.attrib["row"] = str(data["row"])
-                elem.attrib["column"] = str(data["column"])
-                elem.attrib["s"] = data["formula"]
-                elem.attrib["valid"] = "true" if data["valid"] else "false"
-                if data["valid"]:
-                    if "formula_tokens" in data:
-                        elem.attrib["token-count"] = str(len(data["formula_tokens"]))
-                        for token in data["formula_tokens"]:
-                            elem_token = ET.SubElement(elem, "token")
-                            elem_token.attrib["s"] = token[0]
-                            elem_token.attrib["type"] = token[1]
-                else:
-                    # invalid formula
-                    elem.attrib["error"] = data["error"]
-
-    outpath = f"{filepath}{FORMULAS_FILENAME_XML}"
-    with open(outpath, "w") as f:
-        s = ET.tostring(elem_doc, "utf-8").decode("utf-8")
-        f.write(s)
-
-    return output_buffer
 
 
 def main():
